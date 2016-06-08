72e525dd0e20d7f64ac735db0b14b37aef95b94b
Tom Deakin <tom.deakin@bristol.ac.uk>
2016-06-08 04:48:50 -0500

    Have Large, Medium and Small arrays

--- mega-stream.c
+++ mega-stream.c
@@ -12,50 +12,73 @@
 
 int main(void)
 {
-  const unsigned int elements = 100000000;
-  const size_t size = sizeof(double)*elements;
-  double *a = malloc(size);
-  double *b = malloc(size);
-  double *c = malloc(size);
-  double *d = malloc(size);
-  double *e = malloc(size);
-  double *f = malloc(size);
-  double *g = malloc(size);
-  double *h = malloc(size);
-  double *r = malloc(size);
+
+  const int ntimes = 10;
+
+  const unsigned int L_size = 200000000;
+  const unsigned int M_size =  10000000;
+  const unsigned int S_size =       100;
+
+  const double size = 8.0 * (2.0*L_size + 3.0*M_size + 3.0*S_size) * 1.0E-6;
+
+  double *q = malloc(sizeof(double)*L_size);
+  double *r = malloc(sizeof(double)*L_size);
+
+  double *x = malloc(sizeof(double)*M_size);
+  double *y = malloc(sizeof(double)*M_size);
+  double *z = malloc(sizeof(double)*M_size);
+
+  double *a = malloc(sizeof(double)*S_size);
+  double *b = malloc(sizeof(double)*S_size);
+  double *c = malloc(sizeof(double)*S_size);
 
   /* Initalise the data */
-  #pragma omp parallel for
-  for (unsigned int i = 0; i < elements; i++)
+  #pragma omp parallel
   {
-    a[i] = 0.1;
-    b[i] = 0.2;
-    c[i] = 0.3;
-    d[i] = 0.4;
-    e[i] = 0.5;
-    f[i] = 0.6;
-    g[i] = 0.7;
-    h[i] = 0.8;
-    r[i] = 0.0;
+    #pragma omp for
+    for (unsigned int i = 0; i < L_size; i++)
+    {
+      q[i] = 0.1;
+      r[i] = 0.0;
+    }
+
+    #pragma omp for
+    for (unsigned int i = 0; i < M_size; i++)
+    {
+      x[i] = 0.2;
+      y[i] = 0.3;
+      z[i] = 0.4;
+    }
+
+    #pragma omp for
+    for (unsigned int i = 0; i < S_size; i++)
+    {
+      a[i] = 0.6;
+      b[i] = 0.7;
+      c[i] = 0.8;
+    }
+
   }
 
-
-  for (int t = 0; t < 10; t++)
+  /* Run the kernel multiple times */
+  for (int t = 0; t < ntimes; t++)
   {
     double tick = omp_get_wtime();
     /* Kernel */
     #pragma omp parallel for
-    for (unsigned int i = 0; i < elements; i++)
+    for (unsigned int i = 0; i < L_size; i++)
     {
-      r[i] = a[i]*b[i] + c[i]*d[i] + e[i]*f[i] + g[i]*h[i];
+      r[i] = q[i] + a[i%S_size]*x[i%M_size] + b[i%S_size]*y[i%M_size] + c[i%S_size]*z[i%M_size];
     }
     double tock = omp_get_wtime();
     printf("Iteration %d took %lf\n", t, tock-tick);
+    printf("Bandwidth: %lf MB/s\n", size/(tock-tick));
+
   }
 
   /* Check the results */
-  double gold = 0.1*0.2 + 0.3*0.4 + 0.5*0.6 + 0.7*0.8;
-  for (unsigned int i = 0; i < elements; i++)
+  double gold = 0.1 + 0.2*0.6 + 0.3*0.7 + 0.4*0.8;
+  for (unsigned int i = 0; i < L_size; i++)
   {
     if (r[i] != gold)
     {
@@ -65,14 +88,13 @@ int main(void)
   }
 
   /* Free memory */
+  free(q);
+  free(r);
+  free(x);
+  free(y);
+  free(z);
   free(a);
   free(b);
   free(c);
-  free(d);
-  free(e);
-  free(f);
-  free(g);
-  free(h);
-  free(r);
 
 }
