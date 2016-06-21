7db0fd9e0eddcfe4d2ebfefab5b71c7eb9c76050
Tom Deakin <tom.deakin@bristol.ac.uk>
2016-06-21 09:21:51 +0200

    Add a Fortran version

---
+++ mega-stream.f90
@@ -0,0 +1,65 @@
+PROGRAM megastreamftn
+
+  USE omp_lib
+
+  IMPLICIT NONE
+  
+  REAL(8), ALLOCATABLE, DIMENSION(:,:) :: r, q
+  REAL(8), ALLOCATABLE, DIMENSION(:)   :: a, x
+
+  REAL(8), ALLOCATABLE, DIMENSION(:) :: times
+
+  REAL(8) :: size
+ 
+  INTEGER :: L_size, S_size
+
+  INTEGER :: i, t, ntimes
+
+  DOUBLE PRECISION :: tick, tock
+
+  S_size = 128
+  L_size = 134217728 / S_size
+
+  ntimes = 10
+
+  size = 8.0 * (2.0*L_size*S_size + 2.0*S_size) * 1.0E-6
+  
+  ALLOCATE(r(S_size,L_size))
+  ALLOCATE(q(S_size,L_size))
+  ALLOCATE(a(S_size))
+  ALLOCATE(x(S_size))
+  ALLOCATE(times(ntimes))
+
+!$OMP PARALLEL WORKSHARE
+  r = 0.0_8
+  q = 0.1_8
+  x = 0.2_8
+  a = 0.6_8
+!$OMP END PARALLEL WORKSHARE
+
+  iters: DO t = 1, ntimes
+
+    tick = omp_get_wtime()
+
+    !$OMP PARALLEL DO
+      DO i = 1, L_size
+        r(:,i) = q(:,i) + a(:)*x(:)
+      END DO
+    !$OMP END PARALLEL DO
+
+    tock = omp_get_wtime()
+
+    times(t) = tock-tick
+
+  END DO iters
+
+  print *, size/MINVAL(times(2:))
+  
+  DEALLOCATE(r)
+  DEALLOCATE(q)
+  DEALLOCATE(a)
+  DEALLOCATE(x)
+  DEALLOCATE(times)
+
+END PROGRAM megastreamftn
+
