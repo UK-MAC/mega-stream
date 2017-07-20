6cd6b557f12dc64faf93377999238b72fbbf10d3
Tom Deakin <tom.deakin@bristol.ac.uk>
2017-07-20 22:06:49 +0100

    Add Licence

--- mega-sweep.f90
+++ mega-sweep.f90
@@ -1,5 +1,29 @@
-! mega-stream within a KBA sweep
+!
+!
+! Copyright 2017 Tom Deakin, University of Bristol
+!
+! This file is part of mega-stream.
+!
+! mega-stream is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! mega-stream is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with mega-stream.  If not, see <http://www.gnu.org/licenses/>.
+!
+! This aims to investigate the limiting factor for a simple kernel, in particular
+! where bandwidth limits not to be reached, and latency becomes a dominating factor.
+
+!
 program megasweep
+
+  ! MEGA-SWEEP adds a KBA sweep along with the MPI communication to the MEGA-STREAM kernel
 
   use mpi
 
